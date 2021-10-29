0d25af31def2778e9d6afc5ebb4ac8cb26515a07
simorgh1 <bahi@hey.com>
2021-10-29 18:32:06 +0200

    .gitconfig and default aws region added

--- start.sh
+++ start.sh
@@ -6,11 +6,15 @@ if [ ! -d "~/.config" ]; then
 	mkdir -p ~/.config
 fi
 
+CONTAINERHOME="/home/coder"
+
 docker run -it --rm --name code-server --hostname code-server -p 127.0.0.1:8080:8080 \
-	-v "$PWD:/home/coder/project" \
-	-v "${HOME}/.config:/home/coder/.config" \
-	-v "${HOME}/.aws:/home/coder/.aws:ro" \
-	-w "/home/coder/project" \
+	-v "$PWD:$CONTAINERHOME/project" \
+	-v "$HOME/.config:$CONTAINERHOME/.config" \
+	-v "$HOME/.aws:$CONTAINERHOME/.aws:ro" \
+	-v "$HOME/.gitconfig:$CONTAINERHOME/.gitconfig:ro" \
+	-e "AWS_DEFAULT_REGION=us-east-1" \
+	-w "$CONTAINERHOME/project" \
 	-u "$(id -u):$(id -g)" \
 	-e "DOCKER_USER=$USER" \
 	codercom/code-server:latest
