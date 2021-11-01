f73114501c589893334731664aca9a8c7d2b5e3d
simorgh1 <bahi@hey.com>
2021-11-01 12:48:41 +0100

    .docker added
    Docker related files, moved here. Variable BindPort added.

--- .docker/library-scripts/dev-tools.sh
+++ .docker/library-scripts/dev-tools.sh
@@ -2,11 +2,16 @@
 
 set -eu
 
+# Prepare latest node installation
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | bash - && \
+
 #### Install required tools
 
 apt-get update -y && \
 export DEBIAN_FRONTEND=noninteractive && \
 apt-get -y install --no-install-recommends \
+nodejs \
 python3 \
 zip \
 unzip \
@@ -14,3 +19,7 @@ make \
 xdg-utils \
 python3-pip && \
 pip3 install shyaml cfn-lint
+
+# Remove old node
+
+rm /usr/lib/code-server/node
--- start.sh
+++ start.sh
@@ -15,14 +15,25 @@ if [ ! -d "~/.local/share/code-server" ]; then
 fi
 
 CONTAINERHOME="/home/coder"
+BINDPORT=${1:-"8080"}
 
 # Build with additional toolset
 
-docker build -t my/code-server .
+docker build -t my/code-server-aws \
+	     -f .docker/Dockerfile \
+	     --pull \
+	     --label "name=my/code-server-aws:1.0.0" .
+
+# Scan build, docker login required
+
+# docker scan --accept-license -f .docker/Dockerfile my/code-server
 
 # Run customizd code-server
 
-docker run -it --rm --name code-server --hostname code-server -p 127.0.0.1:8080:8080 \
+docker run -it --rm \
+	--name code-server-aws \
+	--hostname code-server-aws \
+	-p 127.0.0.1:${BINDPORT}:${BINDPORT} \
 	-v "$PWD:$CONTAINERHOME/project" \
 	-v "$HOME/.config:$CONTAINERHOME/.config" \
 	-v "$HOME/.local/share/code-server:$CONTAINERHOME/.local/share/code-server" \
@@ -32,5 +43,6 @@ docker run -it --rm --name code-server --hostname code-server -p 127.0.0.1:8080:
 	-w "$CONTAINERHOME/project" \
 	-u "$(id -u):$(id -g)" \
 	-e "DOCKER_USER=$USER" \
-	my/code-server:latest
+        -e "BindAddress=0.0.0.0:${BINDPORT}" \
+	my/code-server-aws:latest
 	
