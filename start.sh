908335ff4c1716380bc85066628621f32f0df768
simorgh1 <bahi@hey.com>
2021-11-01 16:06:01 +0100

    tls startup mode added
    Running with tls, custom domain and github as idp

--- start.sh
+++ start.sh
@@ -27,35 +27,25 @@ docker build -t my/code-server-aws \
 
 # Run customizd code-server
 
-if [ "$TLS" == "tls" ]; then # Run with https and custom domain
-	docker run -it --rm \
-		--name code-server-aws \
-		--hostname code-server-aws \
-		-v "$PWD:$CONTAINERHOME/project" \
-		-v "$HOME/.config:$CONTAINERHOME/.config" \
-		-v "$HOME/.local/share/code-server:$CONTAINERHOME/.local/share/code-server" \
-		-v "$HOME/.aws:$CONTAINERHOME/.aws:ro" \
-		-v "$HOME/.gitconfig:$CONTAINERHOME/.gitconfig:ro" \
-		-e "AWS_DEFAULT_REGION=us-east-1" \
-		-w "$CONTAINERHOME/project" \
-		-u "$(id -u):$(id -g)" \
-		-e "DOCKER_USER=$USER" \
-		-e "TLS=--link" \
-		my/code-server-aws:latest
+if [ "$TLS" == "tls" ]; then
+	# Run with tls, custom domain and GitHub as identity provider
+	Binding="--env TLS=--link"
 else
-	docker run -it --rm \ # Run local listening on BindPort
-		--name code-server-aws \
-		--hostname code-server-aws \
-		-p 127.0.0.1:${BINDPORT}:${BINDPORT} \
-		-v "$PWD:$CONTAINERHOME/project" \
-		-v "$HOME/.config:$CONTAINERHOME/.config" \
-		-v "$HOME/.local/share/code-server:$CONTAINERHOME/.local/share/code-server" \
-		-v "$HOME/.aws:$CONTAINERHOME/.aws:ro" \
-		-v "$HOME/.gitconfig:$CONTAINERHOME/.gitconfig:ro" \
-		-e "AWS_DEFAULT_REGION=us-east-1" \
-		-w "$CONTAINERHOME/project" \
-		-u "$(id -u):$(id -g)" \
-		-e "DOCKER_USER=$USER" \
-		-e "BindAddress=0.0.0.0:${BINDPORT}" \
-		my/code-server-aws:latest
+	# Default run local listening on BindPort
+	Binding="-p 127.0.0.1:${BINDPORT}:${BINDPORT} --env BindAddress=0.0.0.0:${BINDPORT}"
 fi
+
+docker run -it --rm \
+	--name code-server-aws \
+	--hostname code-server-aws \
+	-v "$PWD:$CONTAINERHOME/project" \
+	-v "$HOME/.config:$CONTAINERHOME/.config" \
+	-v "$HOME/.local/share/code-server:$CONTAINERHOME/.local/share/code-server" \
+	-v "$HOME/.aws:$CONTAINERHOME/.aws:ro" \
+	-v "$HOME/.gitconfig:$CONTAINERHOME/.gitconfig:ro" \
+	-e "AWS_DEFAULT_REGION=us-east-1" \
+	-w "$CONTAINERHOME/project" \
+	-u "$(id -u):$(id -g)" \
+	-e "DOCKER_USER=$USER" \
+	$Binding \
+	my/code-server-aws:latest
